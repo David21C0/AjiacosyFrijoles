e4a151f4f9f4418911c1265a8a159d2e7ef25b43
David21C0 <davidcuellar971@gmail.com>
2025-07-01 15:39:27 -0500

    9

--- infrastructure/Telegram/telegram.py
+++ infrastructure/Telegram/telegram.py
@@ -9,9 +9,15 @@ def enviar_mensaje_telegram(chat_id: int, texto: str):
         "chat_id": chat_id,
         "text": texto
     }
+
+    print(f"[DEBUG] Enviando a chat_id={chat_id} el texto: {texto}")
+    print(f"[DEBUG] URL: {url}")
+    print(f"[DEBUG] Payload: {payload}")
+
     response = requests.post(url, json=payload)
 
     if not response.ok:
         print(f"[ERROR] No se pudo enviar el mensaje a Telegram: {response.text}")
     
     return response.json()
+

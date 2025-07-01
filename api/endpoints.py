2c093c2f0212b9a28441d4bd44635464db0089bf
David21C0 <davidcuellar971@gmail.com>
2025-07-01 14:17:56 -0500

    3

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from pydantic import BaseModel
 from services.conversation_service import process_message
 
@@ -13,6 +13,10 @@ async def root():
     return {"mensaje": "Hola desde el asistente virtual del restaurante"}
 
 @router.post("/mensaje")
-async def recibir_mensaje(mensaje: Mensaje):
+async def recibir_mensaje(request: Request):
+    body = await request.json()
+    print("Cuerpo crudo del request:", body)
+    # Si quieres usar el modelo después:
+    mensaje = Mensaje(**body)
     resultado = process_message(mensaje.phone, mensaje.mensaje)
     return resultado
